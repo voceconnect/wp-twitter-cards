93a0cedb5c5febd4a8c0fc326a5428ad54b7fa8f
Kevin Langley Jr <klangley@voceconnect.com>
2013-06-17 15:16:52 -0400

    adding settings for title and description and allowing those to take precendence over the post title or the excerpt. adding filter to separately disable the title and description settings. adding checks for dependent plugins. and adding product card.

--- wp-twitter-cards.php
+++ wp-twitter-cards.php
@@ -6,12 +6,16 @@ class WP_Twitter_Cards {
 		'gallery' => 'Gallery',
 		'photo' => 'Photo',
 		'summary_large_image' => 'Summary Large Image',
-		'summary' => 'Summary'
+		'summary' => 'Summary',
+		'product' => 'Product'
 	);
 
 	static $post_types = array();
 
 	public static function init(){
+		if ( !class_exists( 'Voce_Meta_API' ) )
+			return _doing_it_wrong( __FUNCTION__, 'The Voce Post Meta plugin must be active for the Twitter Card integration to work correctly.', '' );
+
 		add_action( 'add_meta_boxes', array( __CLASS__, 'add_meta_boxes' ), 11, 2 );
 		add_action( 'wp_head', array( __CLASS__, 'render_card_meta' ) );
 
@@ -20,6 +24,11 @@ class WP_Twitter_Cards {
 
 			add_metadata_group( $post_type . '_twitter_card', 'Twitter Card' );
 
+
+			if( !apply_filters( 'twitter_card_title_setting_disabled', false ) ){
+				add_metadata_field( $post_type . '_twitter_card', 'twitter_card_title', 'Card Title' );
+			}
+
 			add_metadata_field( $post_type . '_twitter_card', 'twitter_card_type', 'Card Type', 'dropdown', array(
 				'options' => array_merge(
 					array( '' => 'None' ),
@@ -27,22 +36,56 @@ class WP_Twitter_Cards {
 				)
 			) );
 
-			if( in_array( 'gallery', array_keys( $card_types ) ) ){
-				for($i=1; $i <= 4; $i++){
+			if( !apply_filters( 'twitter_card_description_setting_disabled', false ) ){
+				add_metadata_field( $post_type . '_twitter_card', 'twitter_card_description', 'Card Description', 'textarea' );
+			}
+
+			if( count( array_intersect( array( 'gallery', 'summary_large_image', 'product' ), array_keys( $card_types ) ) ) ){
+				if ( !class_exists( 'MultiPostThumbnails' ) )
+					return _doing_it_wrong( __FUNCTION__, 'The Multi Post Thumbnails plugin must be active for some twitter card types to work correctly.', '' );
+
+				if( in_array( 'gallery', array_keys( $card_types ) ) ){
+
+					for($i=1; $i <= 4; $i++){
+						new MultiPostThumbnails( array(
+							'label' => 'Twitter Gallery Image #' . $i,
+							'id' => 'twitter-gallery-image-' . $i,
+							'post_type' => $post_type
+						) );
+					}
+				}
+
+				if( in_array( 'summary_large_image', array_keys( $card_types ) ) ){
+
 					new MultiPostThumbnails( array(
-						'label' => 'Twitter Gallery Image #' . $i,
-						'id' => 'twitter-gallery-image-' . $i,
+						'label' => 'Twitter Large Image',
+						'id' => 'twitter-large-image',
 						'post_type' => $post_type
 					) );
 				}
-			}
 
-			if( in_array( 'summary_large_image', array_keys( $card_types ) ) ){
-				new MultiPostThumbnails( array(
-					'label' => 'Twitter Large Image',
-					'id' => 'twitter-large-image',
-					'post_type' => $post_type
-				) );
+				if( in_array( 'product', array_keys( $card_types ) ) ){
+
+					add_metadata_field( $post_type . '_twitter_card', 'twitter_card_label1', 'Card Label 1', 'text', array(
+						'description' => 'ex. Price'
+					) );
+					add_metadata_field( $post_type . '_twitter_card', 'twitter_card_data1', 'Card Data 1', 'text', array(
+						'description' => 'ex. $3.00'
+					) );
+
+					add_metadata_field( $post_type . '_twitter_card', 'twitter_card_label2', 'Card Label 2', 'text', array(
+						'description' => 'ex. Color'
+					) );
+					add_metadata_field( $post_type . '_twitter_card', 'twitter_card_data2', 'Card Data 2', 'text', array(
+						'description' => 'ex. Black'
+					) );
+
+					new MultiPostThumbnails( array(
+						'label' => 'Twitter Product Image',
+						'id' => 'twitter-product-image',
+						'post_type' => $post_type
+					) );
+				}
 			}
 		}
 	}
@@ -56,23 +99,43 @@ class WP_Twitter_Cards {
 		}
 	}
 
-	public static function add_meta_boxes($post_type, $post){
-		error_log($post_type);
+	static function get_the_title(){
+		$title_setting_val = Voce_Meta_API::GetInstance()->get_meta_value( get_queried_object_id(), get_post_type() . '_twitter_card', 'twitter_card_title' );
+		return apply_filters( 'twitter_card_title_setting_disabled', false ) ? get_the_title() : ( !empty( $title_setting_val ) ? $title_setting_val : get_the_title() );
+	}
+
+	static function get_the_description(){
+		$description_setting_val = Voce_Meta_API::GetInstance()->get_meta_value( get_queried_object_id(), get_post_type() . '_twitter_card', 'twitter_card_description' );
+		$description = ( has_excerpt() ) ? get_the_excerpt() : wp_trim_words( strip_shortcodes( strip_tags( get_post( get_queried_object_id() )->post_content ) ), 40, '...' );
+		return apply_filters( 'twitter_card_description_setting_disabled', false ) ? $description : ( !empty( $description_setting_val ) ? $description_setting_val : $description );
+	}
+
+	static function add_meta_boxes($post_type, $post){
 		if( !post_type_supports( $post_type, $post_type . '_twitter_card' ) )
 			return;
 
 		$card_type = Voce_Meta_API::GetInstance()->get_meta_value( $post->ID, $post_type . '_twitter_card', 'twitter_card_type' );
-		if( $card_type != 'gallery' ){
-			for($i=1; $i <= 4; $i++){
-				remove_meta_box( sprintf( '%s-twitter-gallery-image-%d', $post_type, $i ), $post_type, 'side' );
+
+		foreach( array( 'normal', 'advanced', 'side' ) as $context ){
+			if( $card_type != 'gallery' ){
+				for($i=1; $i <= 4; $i++)
+					remove_meta_box( sprintf( '%s-twitter-gallery-image-%d', $post_type, $i ), $post_type, $context );
+			}
+			if( $card_type != 'summary_large_image' ){
+				remove_meta_box( sprintf( '%s-twitter-large-image', $post_type ), $post_type, $context );
+			}
+			if( $card_type != 'product' ){
+				remove_meta_box( sprintf( '%s-twitter-product-image', $post_type ), $post_type, $context );
+				for( $i=1; $i<=2; $i++ ){
+					remove_metadata_field( $post_type . '_twitter_card', 'twitter_card_label' . $i );
+					remove_metadata_field( $post_type . '_twitter_card', 'twitter_card_data' . $i );
+				}
 			}
 		}
-		if( $card_type != 'summary_large_image' ){
-			remove_meta_box( sprintf( '%s-twitter-large-image', $post_type ), $post_type, 'side' );
-		}
+
 	}
 
-	public static function render_card_meta(){
+	static function render_card_meta(){
 		if( !is_singular( array_keys( self::$post_types ) ) )
 			return;
 
@@ -81,37 +144,43 @@ class WP_Twitter_Cards {
 
 		$card_data = array(
 			'card' => $card_type,
-			'title' => get_the_title(),
-			'description' => ( has_excerpt() ) ? get_the_excerpt() : wp_trim_words( strip_shortcodes( strip_tags( get_post( get_queried_object_id() )->post_content ) ), 40, '...' )
+			'title' => self::get_the_title(),
+			'description' => self::get_the_description()
 		);
 
 		switch( $card_type ){
 			case 'gallery':
+
 				$images = array();
 				for($i=1; $i <= 4; $i++){
-					if( $image_id = MultiPostThumbnails::get_post_thumbnail_id( get_post_type(), 'twitter-gallery-image-' . $i, get_queried_object_id() ) ){
-						if( $image = wp_get_attachment_image_src( $image_id, 'medium' ) ){
-							$images['image' . ($i - 1)] = $image[0];
-						}
+					if( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'twitter-gallery-image-' . $i, get_queried_object_id() ) ){
+						$images['image' . ($i - 1)] = MultiPostThumbnails::get_post_thumbnail_url( get_post_type(), 'twitter-gallery-image-' . $i, get_queried_object_id(), 'medium' );
 					}
 				}
+
 				if( !empty( $images ) )
 					$card_data = array_merge( $card_data, $images );
+
 			break;
 			case 'summary_large_image':
-				if( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'twitter-large-image', get_queried_object_id() ) ){
-					$image = wp_get_attachment_image_src( MultiPostThumbnails::get_post_thumbnail_id( get_post_type(), 'twitter-large-image', get_queried_object_id() ), 'medium' );
-					if( $image )
-						$card_data['image'] = $image[0];
-				}
+
+				if( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'twitter-large-image', get_queried_object_id() ) )
+					$card_data['image'] = MultiPostThumbnails::get_post_thumbnail_url( get_post_type(), 'twitter-large-image', get_queried_object_id(), 'large' );
+
+			break;
+			case 'product':
+				if( MultiPostThumbnails::has_post_thumbnail( get_post_type(), 'twitter-product-image', get_queried_object_id() ) )
+					$card_data['image'] = MultiPostThumbnails::get_post_thumbnail_url( get_post_type(), 'twitter-product-image', get_queried_object_id(), 'large' );
 			break;
 			case 'photo':
 			case 'summary':
+
 				if( has_post_thumbnail() ){
 					$image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'medium' );
 					if( $image )
 						$card_data['image'] = $image[0];
 				}
+
 			break;
 		}
 
@@ -123,12 +192,11 @@ class WP_Twitter_Cards {
 
 		if( $card_type == 'gallery' ){
 			for( $i=0; $i<4; $i++ ){
-				if( !isset( $card_data['image' . $i] ) || empty( $card_data['image' . $i] ) ){
+				if( !isset( $card_data['image' . $i] ) || empty( $card_data['image' . $i] ) )
 					return;
-				}
 			}
 		} else if( $card_type == 'photo' ){
-			if( !isset( $card_type['image'] ) || empty( $card_type['image'] ) )
+			if( !isset( $card_data['image'] ) || empty( $card_data['image'] ) )
 				return;
 		}
 
