57292b306bf1c64b4ba85b61aa943784477655cf
Mat Gargano <mgargano@gmail.com>
2014-03-21 16:57:14 -0400

    Adding unit tests

---
+++ tests/test_wp_twitter_cards.php
@@ -0,0 +1,17 @@
+<?php
+
+class WP_Twitter_cards_Tests extends WP_UnitTestCase {
+ 
+    function test_true_is_true() {
+        
+        $this->assertTrue( true );
+    } 
+    
+	function test_false_is_false() {
+        
+        $this->assertFalse( false );
+    } 
+    
+
+
+} 
